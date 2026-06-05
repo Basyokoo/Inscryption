160891e62f34358f3ac6869014e8de4775a9259e
Baptiste Metz <baptistemetz90@gmail.com>
2026-06-05 15:08:02 +0200

    Fix de l'affichage et du mécanisme du bot + ajout de la jouabilité des pouvoirs + ajout d'affichage pour les fin de manche et de partie et ajout d'élément mineurs pour l'affichage. Fix de la gestion du combat et des choix lors de la partie + ajout affichage de logs

--- src/jeu/logic/Score.java
+++ src/jeu/logic/Score.java
@@ -1,71 +1,49 @@
 package jeu.logic;
 
-import jeu.model.Carte;
-
 public class Score {
-    private int m_scoreJoueur;
-    private int m_scoreEnnemi;
+    private int m_balanceScore;
 
     public Score() {
-        this.m_scoreEnnemi = 0;
-        this.m_scoreJoueur = 0;
+        this.m_balanceScore = 0;
     }
+
     public void ajouterPointsJoueur(int points) {
         if (points > 0) {
-            this.m_scoreJoueur += points;
-            this.m_scoreEnnemi -= points;
+            this.m_balanceScore += points;
         }
     }
+
     public void ajouterPointsEnnemi(int points) {
         if (points > 0) {
-            this.m_scoreEnnemi += points;
-            this.m_scoreJoueur -= points;
+            this.m_balanceScore -= points;
         }
     }
 
-    public int getValeurEcart(){
-        return this.m_scoreJoueur - this.m_scoreEnnemi;
+    public int getValeurEcart() {
+        return this.m_balanceScore;
     }
 
-    public boolean estVictoireJoueur(){
-        return this.m_scoreJoueur >= 5; // Respecte la consigne de déséquilibre de 5
+    public boolean estVictoireJoueur() {
+        return this.m_balanceScore >= 5;
     }
 
-    public boolean estVictoireEnnemi(){
-        return this.m_scoreEnnemi <= -5; // Respecte la consigne de déséquilibre de 5
+    public boolean estVictoireEnnemi() {
+        return this.m_balanceScore <= -5;
     }
 
-    public String getScoreJoueur(){
-        return String.valueOf(this.m_scoreJoueur);
+    public String getScoreJoueur() {
+        return String.valueOf(this.m_balanceScore > 0 ? this.m_balanceScore : 0);
     }
 
-    public String getScoreEnnemi(){
-        return String.valueOf(this.m_scoreEnnemi);
+    public String getScoreEnnemi() {
+        return String.valueOf(this.m_balanceScore < 0 ? Math.abs(this.m_balanceScore) : 0);
     }
 
     public void setValeurJoueur(int nouvelleValeur) {
-        this.m_scoreJoueur = nouvelleValeur;
+        this.m_balanceScore = nouvelleValeur;
     }
 
     public void setValeurEnnemi(int nouvelleValeur) {
-        this.m_scoreEnnemi = nouvelleValeur;
-    }
-
-    public void gererScoreJoueur(java.util.ArrayList<jeu.model.Carte> ligneJeu, int nbOs, int nbSang) {
-        int nouveauScore = 0;
-        for (jeu.model.Carte c : ligneJeu) {
-            if (c != null && c.estAnimal()) {
-                nouveauScore += c.getAttack() + c.getVie();
-            }
-        }
-        nouveauScore += nbOs;
-        nouveauScore += (nbSang * 2);
-
-        // 3. Mise à jour de l'écart interne
-        this.m_scoreJoueur = nouveauScore;
-    }
-
-    public void gererScoreEnnemi(int nbOs){
-        this.ajouterPointsEnnemi(nbOs);
+        this.m_balanceScore = -nouvelleValeur;
     }
 }
