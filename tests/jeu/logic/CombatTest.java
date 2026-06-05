ef550b8ccc2b0f9f99f6854617fe0d8044abce33
Siegel <axelsiegel@icloud.com>
2026-06-05 18:08:50 +0200

    UML corrigé

---
+++ tests/jeu/logic/CombatTest.java
@@ -0,0 +1,10 @@
+package jeu.logic;
+
+import junit.framework.TestCase;
+import jeu.model.Carte;
+import jeu.model.Animal;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class CombatTest extends TestCase {
+}
