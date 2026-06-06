8c8e0b12a7759615d60afb634e6cd492a965dd22
Siegel <axelsiegel@icloud.com>
2026-06-06 09:00:05 +0200

    Rendu 3

--- src/jeu/logic/Score.java
+++ src/jeu/logic/Score.java
@@ -2,23 +2,19 @@ package jeu.logic;
 
 public class Score {
     private int m_balanceScore;
-
     public Score() {
         this.m_balanceScore = 0;
     }
-
     public void ajouterPointsJoueur(int points) {
         if (points > 0) {
             this.m_balanceScore += points;
         }
     }
-
     public void ajouterPointsEnnemi(int points) {
         if (points > 0) {
             this.m_balanceScore -= points;
         }
     }
-
     public int getValeurEcart() {
         return this.m_balanceScore;
     }
@@ -38,7 +34,6 @@ public class Score {
     public String getScoreEnnemi() {
         return String.valueOf(this.m_balanceScore < 0 ? Math.abs(this.m_balanceScore) : 0);
     }
-
     public void setValeurJoueur(int nouvelleValeur) {
         this.m_balanceScore = nouvelleValeur;
     }
